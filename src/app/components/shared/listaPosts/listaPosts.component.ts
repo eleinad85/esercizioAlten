ca0d21e75f96b01f43c0b1a6e9269fa410b238c7
extfferraguto <daniele.carfagna@technodespa.com>
2024-05-15 17:36:05 +0200

    mod

---
+++ src/app/components/shared/listaPosts/listaPosts.component.ts
@@ -0,0 +1,56 @@
+import { NgFor, NgIf } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit, input } from '@angular/core';
+import { forkJoin, map, switchMap } from 'rxjs';
+import { Post } from '../../../../models/post.interface';
+import { User } from '../../../../models/user.interface';
+import { PostService } from '../../../../services/post.service';
+import { UsersService } from '../../../../services/users.service';
+
+@Component({
+  selector: 'app-listaPosts',
+  templateUrl: './listaPosts.component.html',
+  styleUrls: ['./listaPosts.component.css'],
+  standalone: true,
+
+
+})
+export class ListaPostsComponent implements OnInit {
+  tipo = input<string>('')
+  posts: Post[] = []
+  users: User[] = []
+
+  constructor(public postsService: PostService, public usersService: UsersService) { }
+
+  ngOnInit() {
+    this.postsService.getPosts()
+      .pipe(
+        switchMap(
+          posts => this.usersService.getUsers()
+            .pipe(
+              map(users => ({
+                posts, users
+              }))
+            )
+        )
+      )
+      .subscribe(
+        result => {
+          for (let i = 0; i < result.posts.length; i++) {
+            let postUserId = result.posts[i].userId;
+
+            for (let j = 0; j < result.users.length; j++) {
+              let id = result.users[j].id;
+              if (postUserId == id)
+                result.posts[i].user = result.users[j]
+
+            }
+          }
+          this.posts = result.posts
+
+        }
+      )
+
+  }
+
+}
